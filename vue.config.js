6fb0c55a7ee28334cf54a01f3bd1b4d8cedd566f
qa418054097 <418054097@qq.com>
2020-07-23 14:03:35 +0800

    dfssfsd

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ const webpack = require('webpack')
 module.exports = {
   publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : './', // 默认'/'，部署应用包时的基本 URL
   outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
-  assetsDir: "", // 相对于outputDir的静态资源(js、css、img、fonts)目录
+  assetsDir: '', // 相对于outputDir的静态资源(js、css、img、fonts)目录
   lintOnSave: false,
   runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
   parallel: require('os').cpus().length > 1,
